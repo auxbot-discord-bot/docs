b776c568a0df7fcecbd0550016c6832d11f108f0
1chiSensei <mail@tomio.codes>
2021-08-16 15:35:00 +0800

    feat: implement search

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,5 +5,10 @@ export default defineUserConfig<DefaultThemeOptions, ViteBundlerOptions>({
 	bundler: '@vuepress/vite',
 	lang: 'en-US',
 	title: 'AuxBot',
-	description: 'AuxBot is a multi use discord bot which has many different things such as logging, moderation, music, memes, fun, and so many more commands.'
+	description: 'AuxBot is a multi use discord bot which has many different things such as logging, moderation, music, memes, fun, and so many more commands.',
+	plugins: [
+		[
+			'@vuepress/plugin-search'
+		]
+	]
 });
